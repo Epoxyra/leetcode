1131f5abd063def05e91828e2d95c40ce34722f0
Epoxyra <stevmadecdu29@gmail.com>
2025-04-18 14:43:58 +0200

    Optimized solution challenge 20

--- 20. Valid Parentheses/main.py
+++ 20. Valid Parentheses/main.py
@@ -5,17 +5,13 @@ class Solution:
             ']' : '[',
             '}' : '{'
         }
-        open_brackets = ['(', '[', '{']
-        close_brackets = [')', ']', '}']
         stack = []
         for char in s:
-            if char in open_brackets:
-                stack.append(char)
-            if char in close_brackets:
+            if char in parentheses_map:
                 if len(stack) != 0 and parentheses_map[char] == stack[-1]:
                     stack.pop(-1)
                 else:
                     return False
-        if len(stack) != 0:
-            return False
-        return True
+            else:
+                stack.append(char)
+        return not stack
