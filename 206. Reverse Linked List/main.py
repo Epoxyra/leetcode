63ea5ae7eb0dc8ba2f4d87b88b931071f54055de
Epoxyra <stevmadecdu29@gmail.com>
2025-05-05 19:23:17 +0200

    Add challenge 206 optimized solution

--- 206. Reverse Linked List/main.py
+++ 206. Reverse Linked List/main.py
@@ -8,20 +8,10 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        all_nodes = []
-        node = head
-        
-        while node:
-            tmp = node.next
-            node.next = None
-            all_nodes.append(node)
-            node = tmp
-        # Si la liste est vide, retournez None
-        if not all_nodes:
-            return None
-        reversed_list = all_nodes[-1]
-        res = reversed_list
-        for i in range(len(all_nodes) - 2, -1, -1):
-            reversed_list.next = all_nodes[i]
-            reversed_list = reversed_list.next
-        return res
+        node = None
+        while head:
+            tmp = head.next
+            head.next = node
+            node = head
+            head = tmp
+        return node
