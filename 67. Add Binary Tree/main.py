df43bf594854146c03fbf53cb35ecaf78e51d2bf
Epoxyra <stevmadecdu29@gmail.com>
2025-05-11 23:50:10 +0200

    Use zfill method and clean the code for challenge 67

--- 67. Add Binary Tree/main.py
+++ 67. Add Binary Tree/main.py
@@ -1,23 +1,14 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        if len(a) < len(b):
-            a = (len(b) - len(a))*"0" + a
-        elif len(b) < len(a):
-            b = (len(a) - len(b))*"0" + b
+        max_len = max(len(a), len(b))
+        a = a.zfill(max_len)
+        b = b.zfill(max_len)
         stock = 0
         res = []
-        a = a[::-1]
-        b = b[::-1]
-        for i in range(len(a)):
+        for i in range(max_len - 1, -1, -1):
             res.append(str((int(a[i]) + int(b[i]) + stock) % 2))
             stock = (int(a[i]) + int(b[i]) + stock) // 2
         if stock:
             res.append("1")
         res = res[::-1]
         return "".join(res)
-
-a = "11"
-b = "1"
-sol = Solution()
-res = sol.addBinary(a, b)
-print(res)
