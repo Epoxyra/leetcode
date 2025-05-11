8531386ec707619f4f1f4c79a194b3363c2580f1
Epoxyra <stevmadecdu29@gmail.com>
2025-05-11 23:42:02 +0200

    Add better and simpler solution for challenge 67

--- 67. Add Binary Tree/main.py
+++ 67. Add Binary Tree/main.py
@@ -4,26 +4,13 @@ class Solution:
             a = (len(b) - len(a))*"0" + a
         elif len(b) < len(a):
             b = (len(a) - len(b))*"0" + b
-        stock = False
+        stock = 0
         res = []
         a = a[::-1]
         b = b[::-1]
         for i in range(len(a)):
-            if stock:
-                if int(a[i]) and not int(b[i]) or int(b[i]) and not int(a[i]):
-                    stock = True
-                    res.append("0")
-                else:
-                    stock = True if int(a[i]) and int(b[i]) else False
-                    res.append("1")
-            else:
-                print(int(a[i]), int(b[i]))
-                if int(a[i]) and not int(b[i]) or int(b[i]) and not int(a[i]):
-                    stock = False
-                    res.append("1")
-                else:
-                    stock = True if int(a[i]) and int(b[i]) else False
-                    res.append("0")
+            res.append(str((int(a[i]) + int(b[i]) + stock) % 2))
+            stock = (int(a[i]) + int(b[i]) + stock) // 2
         if stock:
             res.append("1")
         res = res[::-1]
