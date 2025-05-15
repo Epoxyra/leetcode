d5ec907dde8318949571b7c8682260038f9fadf8
Epoxyra <stevmadecdu29@gmail.com>
2025-05-15 11:21:23 +0200

    Add challenge 2 clean solution

--- 2. Add Two Numbers/main.py
+++ 2. Add Two Numbers/main.py
@@ -7,32 +7,18 @@ class ListNode:
         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        node = l1
-        l1_numbers = []
-        while node:
-            l1_numbers.append(node.val)
-            node = node.next
-        
-        node = l2
-        l2_numbers = []
-        while node:
-            l2_numbers.append(node.val)
-            node = node.next
-
-        l1_numbers = l1_numbers[::-1]
-        l2_numbers = l2_numbers[::-1]
-
-        l1_str = "".join([str(x) for x in l1_numbers])
-        l2_str = "".join([str(x) for x in l2_numbers])
-        res_str = str(int(l1_str) + int(l2_str))
-        res_numbers = [int(digit) for digit in res_str][::-1]
-
         head = ListNode()
         node = head
-        for i in range(len(res_numbers)):
-            node.val = res_numbers[i]
-            if i + 1 <= len(res_numbers) - 1:
-                node.next = ListNode()
+        carry = 0
+        while l1 or l2 or carry:
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
+            digit = (l1_val + l2_val + carry) % 10
+            carry = (l1_val + l2_val + carry) // 10
+            node.next = ListNode(digit)
+
             node = node.next
-        
-        return head
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return head.next
