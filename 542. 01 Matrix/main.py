d18db6514db7e5333e01a2fea30a804e1be3e7ce
Epoxyra <stevmadecdu29@gmail.com>
2025-05-26 00:02:22 +0200

    Improvement to challenge 542 solution. 49/50 testcases passed

--- 542. 01 Matrix/main.py
+++ 542. 01 Matrix/main.py
@@ -1,23 +1,29 @@
+from collections import deque
+
 class Solution:
     def updateMatrix(self, mat: list[list[int]]) -> list[list[int]]:
         m = len(mat)
         n = len(mat[0])
         res = [[0 for _ in range(n)] for _ in range(m)]
-        def find_zero(x, y, covered = None):
-            if covered is None:
-                covered = set()
-            covered.add((x, y))
-            paths = []
-            if mat[x][y] == 0:
-                return 0
-            for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
-                if 0 <= x + dx < m and 0 <= y + dy < n and (x + dx, y + dy) not in covered:
-                    paths.append(1 + find_zero(x + dx, y + dy, covered.copy()))
-            if paths:
-                return min(paths)
-            else:
-                return float('inf')
+        q = deque()
         for i in range(m):
             for j in range(n):
-                res[i][j] = find_zero(i, j)
+                if mat[i][j] == 0:
+                    res[i][j] = 0
+                else:
+                    q.append((i, j))
+                    covered = []
+                    path_len = 0
+                    while q:
+                        for _ in range(len(q)):
+                            x, y = q.popleft()
+                            if mat[x][y] == 0:
+                                res[i][j] = path_len
+                                q.clear()
+                                break
+                            for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                                if 0 <= x + dx < m and 0 <= y + dy < n and (x + dx, y + dy) not in covered:
+                                    q.append((x + dx, y + dy))
+                                    covered.append((x, y))
+                        path_len += 1
         return res
