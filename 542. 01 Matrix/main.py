585fc8efe2fa8c39d2b3a5a88367a89d15c47b41
Epoxyra <stevmadecdu29@gmail.com>
2025-05-26 15:23:25 +0200

    Add challenge 542 solution

--- 542. 01 Matrix/main.py
+++ 542. 01 Matrix/main.py
@@ -1,29 +1,24 @@
 from collections import deque
-
 class Solution:
     def updateMatrix(self, mat: list[list[int]]) -> list[list[int]]:
         m = len(mat)
         n = len(mat[0])
-        res = [[0 for _ in range(n)] for _ in range(m)]
-        q = deque()
+        res = [[-1 for _ in range(n)] for _ in range(m)]
+        queue = deque()
         for i in range(m):
             for j in range(n):
                 if mat[i][j] == 0:
                     res[i][j] = 0
-                else:
-                    q.append((i, j))
-                    covered = []
-                    path_len = 0
-                    while q:
-                        for _ in range(len(q)):
-                            x, y = q.popleft()
-                            if mat[x][y] == 0:
-                                res[i][j] = path_len
-                                q.clear()
-                                break
-                            for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
-                                if 0 <= x + dx < m and 0 <= y + dy < n and (x + dx, y + dy) not in covered:
-                                    q.append((x + dx, y + dy))
-                                    covered.append((x, y))
-                        path_len += 1
+                    queue.append((i, j))
+        neighbours = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        distance = 0
+        while queue:
+            distance += 1
+            for _ in range(len(queue)):
+                x, y = queue.popleft()
+                for dx, dy in neighbours:
+                    if 0 <= x + dx < m and 0 <= y + dy < n:
+                        if mat[x + dx][y + dy] == 1 and res[x + dx][y + dy] == -1:
+                            res[x + dx][y + dy] = distance
+                            queue.append((x + dx, y + dy))
         return res
